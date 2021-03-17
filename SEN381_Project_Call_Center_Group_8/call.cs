763a6f399d5c472ab96673b5ce979502d3365156
talaat34 <tata.elash34@gmail.com>
2021-03-17 11:45:01 +0200

    Adding Call, IndividualClass

---
+++ SEN381_Project_Call_Center_Group_8/call.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System;
+
+public class call
+{
+    private string callLength;
+    private string callTime;
+    private string indiID;
+    private string busiID;
+    private string empiID;
+
+
+
+    public string CallLength { get => callLength; set => callLength = value; }
+    public string CallTime { get => callTime; set => callTime = value; }
+    public string IndiID { get => indiID; set => indiID = value; }
+    public string BusiID { get => busiID; set => busiID = value; }
+    public string EmpID { get => empiID; set => empiID = value; }
+
+    public call()
+    {
+
+    }
+    public call(string length, string time, string indID, string busID, string empID)
+    {
+        this.callLength = length;
+        this.callTime = time;
+        this.indiID = indID;
+        this.busiID = busID;
+        this.empiID = empID;
+    }
+}
---
+++ SEN381_Project_Call_Center_Group_8/individualClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class individualClient
+{
+    private string indiID;
+    private string name;
+    private string surname;
+    private string role;
+    private string serviceTypeID;
+
+    public string IndiID { get => indiID; set => indiID = value; }
+    public string Name { get => name; set => name = value; }
+    public string Surname { get => surname; set => surname = value; }
+    public string Role { get => role; set => role = value; }
+    public string ServiceTypeID { get => serviceTypeID; set => serviceTypeID = value; }
+
+    public individualClient()
+    {
+    }
+
+    public individualClient(string id, string nm, string surnm, string role, string serviveTyp)
+    {
+        this.indiID = id;
+        this.name = nm;
+        this.surname = surnm;
+        this.role = role;
+        this.serviceTypeID = serviveTyp;
+    }
+
+}
